7803b3992a97f344bc55394ef2fa495d608b3d6f
fehmielmas <mimarfehmielmas@gmail.com>
2024-02-10 23:28:34 +0300

    works

--- Strategy/Assets/Scripts/CoordinateLabeler.cs
+++ Strategy/Assets/Scripts/CoordinateLabeler.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshPro label;
+    private Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
     {
-        
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = "Coordinates (" + coordinates.x + ", " + coordinates.y + ")";
     }
 }
