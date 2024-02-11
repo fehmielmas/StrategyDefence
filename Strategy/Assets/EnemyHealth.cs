b65f91c2d82124f535906d95207f69b9c486eeb6
fehmielmas <mimarfehmielmas@gmail.com>
2024-02-11 13:16:10 +0300

    ce

--- Strategy/Assets/EnemyHealth.cs
+++ Strategy/Assets/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetLocater : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
