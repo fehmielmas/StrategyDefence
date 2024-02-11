9b24caad3b5ef63972edc2ff34e3279c9fc0596c
fehmielmas <mimarfehmielmas@gmail.com>
2024-02-11 13:26:22 +0300

    s

---
+++ Strategy/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints = 10;
+    [SerializeField] int currentHitPoints = 0;
+    
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
