8d58b54d26f4e0bd1b5e4e9f782315d4578bd0d9
fehmielmas <mimarfehmielmas@gmail.com>
2024-02-11 18:42:58 +0300

    c

--- Strategy/Assets/ObjectPool.cs
+++ Strategy/Assets/ObjectPool.cs
@@ -1,18 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] float spawnTimer = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemy());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemy()
     {
-        
+        while (true)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity); // Instantiate at the position of the ObjectPool object
+            yield return new WaitForSeconds(spawnTimer);
+        }
     }
 }
