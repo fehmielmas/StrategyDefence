ab9d18e15a39a95bdeaf261480d800c311841d4f
fehmielmas <mimarfehmielmas@gmail.com>
2024-02-11 21:32:24 +0300

    sec

--- Strategy/Assets/Scripts/TargetLocater.cs
+++ Strategy/Assets/Scripts/TargetLocater.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class TargetLocater : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] ParticleSystem projectileParticle;
+    [SerializeField] float range = 15f;
     [SerializeField] Transform target;
     
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    } 
+    
 
     
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
         
     }
 
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+        
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
+    }
+    
+
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
         transform.LookAt(target);
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
